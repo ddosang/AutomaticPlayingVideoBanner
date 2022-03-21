c03436bb1fa2f1dc9e64411e26a8239771187613
Eunji Lim <eunjilim218@qarasoft.com>
2022-03-21 15:20:48 +0900

    [feat] 카드 자동 재생 추가

--- AutomaticPlayingVideoBanner/ViewController.swift
+++ AutomaticPlayingVideoBanner/ViewController.swift
@@ -13,7 +13,14 @@ class ViewController: UIViewController {
     typealias VideoCell = VideoCollectionViewCell
     typealias ImageCell = ImageCollectionViewCell
     
-    private var cardContents: [String] = ["picka.mov", "0.jpg", "picka.mov", "1.jpg", "picka.mov", "2.jpg", "picka.mov", "0.jpg"]
+    private var cardContents: [String] = ["picka.png", "0.jpg", "picka.mov", "1.jpg", "picka.mov", "2.jpg", "picka.mov", "0.jpg"]
+    
+    private var nowPage: Int = 1 {
+        didSet {
+            setAutoCardTimer()
+        }
+    }
+    private var timer: Timer?
     
     
     lazy var collectionView: UICollectionView = {
@@ -65,12 +72,67 @@ class ViewController: UIViewController {
             make.top.equalTo(collectionView.snp.bottom).offset(10)
             make.left.right.equalToSuperview()
         }
+        
+        // MARK: - Background Observer
+        NotificationCenter.default.addObserver(self, selector: #selector(appMovedToForeground(_:)), name: UIApplication.didBecomeActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(appMovedBackground), name: UIScene.willDeactivateNotification, object: nil)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        collectionView.scrollToItem(at: [0, 1], at: .left, animated: false)
+        nowPage = 1
+        collectionView.scrollToItem(at: [0, nowPage], at: .left, animated: false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        initCardView()
+    }
+    
+    @objc func appMovedToForeground(_ notification: Notification) {
+        setAutoCardTimer()
+        playFirstVisibleVideo()
+    }
+    
+    @objc func appMovedBackground(_ notification: Notification) {
+        // code to execute
+        initCardView()
+    }
+}
+
+extension ViewController {
+    func initCardView() {
+//        collectionView.scrollToItem(at: IndexPath(item: 1, section: 0), at: .left, animated: false)
+//        nowPage = 1
+        timer?.invalidate()
+        timer = nil
+        playFirstVisibleVideo(false)
+    }
+    
+    func setAutoCardTimer() {
+        timer?.invalidate()
+        timer = nil
+        
+        if nowPage == 0 || cardContents[nowPage].hasSuffix("mov") {
+            timer = Timer.scheduledTimer(withTimeInterval: 13.2, repeats: false) { [weak self] (Timer) in
+                self?.cardMove()
+            }
+        } else {
+            timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { [weak self] (Timer) in
+                self?.cardMove()
+            }
+        }
+    }
+
+    func cardMove() {
+        if nowPage == cardContents.count - 2 {
+            collectionView.scrollToItem(at: [0, 1], at: .right, animated: false)
+            nowPage = 1
+            return
+        }
+        
+        nowPage += 1
+        collectionView.scrollToItem(at: [0, nowPage], at: .right, animated: true)
     }
 }
 
@@ -85,17 +147,20 @@ extension ViewController: UIScrollViewDelegate {
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let value = (scrollView.contentOffset.x / scrollView.frame.width)
+        nowPage = Int(round(value))
         
         switch Int(round(value)) {
         case 0:
             let last = cardContents.count - 2
             UIView.animate(withDuration: 0.01, animations: { [weak self] in
-              self?.collectionView.scrollToItem(at: [0, last], at: .left, animated: false)
+                self?.collectionView.scrollToItem(at: [0, last], at: .left, animated: false)
             }, completion: { [weak self] _ in
-              self?.playFirstVisibleVideo()
+                self?.playFirstVisibleVideo()
+                self?.nowPage = last
             })
         case cardContents.count - 1:
-            self.collectionView.scrollToItem(at: [0, 1], at: .left, animated: false)
+            collectionView.scrollToItem(at: [0, 1], at: .left, animated: false)
+            nowPage = 1
         default:
             break
         }
@@ -121,7 +186,7 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-      return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
     }
 }
 
